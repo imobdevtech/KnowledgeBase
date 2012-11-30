6715071d408f34d07730bd634cf201e9fc8ed18f
Jagdish Prajapati <iMobDev@iMobDevs-Mac-mini-2.local>
2012-11-30 21:23:32 +0530

    testing with xib...

--- KnowledgeBase/KnowledgeBase/IMDViewController.m
+++ KnowledgeBase/KnowledgeBase/IMDViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"testing from mac mini");
 }
 
 - (void)didReceiveMemoryWarning
