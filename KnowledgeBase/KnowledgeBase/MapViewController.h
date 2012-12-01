e0befe3c017f44a647e2f9dc23bf1a9487daf4b6
Deepak Patel <support@imobdevtech.com>
2012-12-01 15:22:03 +0530

    Created new Branch to work in new Map Feature

---
+++ KnowledgeBase/KnowledgeBase/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  KnowledgeBase
+//
+//  Created by Deepak Patel on 01/12/12.
+//  Copyright (c) 2012 iMOBDEV Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+
+@end
---
+++ KnowledgeBase/KnowledgeBase/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  KnowledgeBase
+//
+//  Created by Deepak Patel on 01/12/12.
+//  Copyright (c) 2012 iMOBDEV Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
